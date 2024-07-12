7151cc639b0103f090d9c794181c95fc0b4ca6ec
Shaik Alina <shaikalina2004@gmail.com>
2024-07-12 21:31:36 +0530

    Router setup

---
+++ src/landing_page/products/ProductPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Hero from "./Hero";
+import LeftSection from "./LeftSection";
+import RightSection from "./RightSection";
+
+function Signup(){
+    return(
+        <>
+        <Hero />
+        <LeftSection />
+        <RightSection />
+        </>
+    );
+}
+
+export default Signup;
+
---
+++ src/landing_page/support/SupportPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CreateTicket from "./CreateTicket";
+import Hero from "./Hero";
+
+function Signup(){
+    return(
+        <>
+        <CreateTicket />
+        <Hero />
+        </>
+    );
+}
+
+export default Signup;
+
