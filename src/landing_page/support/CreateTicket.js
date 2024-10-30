9fb7f81d7842a74f782edb1d3e29336bec1ab88e
Shaik Alina <shaikalina2004@gmail.com>
2024-10-31 01:32:22 +0530

    working on support page

--- src/landing_page/support/CreateTicket.js
+++ src/landing_page/support/CreateTicket.js
@@ -2,7 +2,71 @@ import React from "react";
 
 function CreateTicket(){
     return(
-        <h1>CreateTicket</h1>
+        <div className="container">
+            <h3 className="text-muted mt-5 p-4 fs-4 fw-normal">To create a ticket, select a relevant topic</h3>
+            <div className="p-5 m-5">
+
+            <div className="row mb-5">
+
+            <div className="col-4">
+                <h4 className="fw-normal"><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;Account opening</h4>
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Getting started</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Online</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Offline</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Charges</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Company, Partnership and HUF</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Non Resident Indian (NRI)</a><br />   
+            </div>
+            <div className="col-4">
+                <h4 className="fw-normal"><i class="fa fa-user" aria-hidden="true"></i>&nbsp;Your Zerodha Account</h4>
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Login credentials</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Your Profile</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Account modification and segment addition</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >CMR & DP ID</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Nomination</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Transfer and conversion of shares</a><br />   
+            </div>
+            <div className="col-4">
+                <h4 className="fw-normal"><i class="fa fa-bar-chart" aria-hidden="true"></i>&nbsp; Trading and Markets</h4 >
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Trading FAQs</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Kite</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Margins</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Product and order types</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Corporate actions</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Kite features</a><br />   
+            </div>
+            </div>
+
+            <div className="row ">
+
+            <div className="col-4">
+                <h4 className="fw-normal"><i class="fa fa-credit-card" aria-hidden="true"></i>&nbsp;Funds</h4 >
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Fund withdrawal</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Adding funds</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Adding bank accounts</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >eMandates</a><br />
+            </div>
+            <div className="col-4">
+                <h4 className="fw-normal"><i class="fa fa-connectdevelop" aria-hidden="true"></i>&nbsp;Console</h4 >
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >IPO</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Funds statement</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Profile</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Reports</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Corporate actions</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Referral program</a><br />   
+            </div>
+            <div className="col-4">
+                <h4 className="fw-normal"><i class="fa fa-circle-thin" aria-hidden="true"></i>&nbsp;Coin</h4>
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Understanding mutual funds and Coin</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Coin app</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Coin web</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >Transactions and reports</a><br />
+                <a href="/" style={{textDecoration :"none" , lineHeight:"2.2rem"}} >National Pension Scheme (NPS)</a><br />
+            </div>
+            </div>
+            </div>
+            </div>
+
     );
 }
 
--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -2,7 +2,52 @@ import React from "react";
 
 function Hero(){
     return(
-        <h1>Hero</h1>
+        <div className="container-fluid" style={{background:'#387ed1' , height:'10%', width:'100%'}}>
+            <div className="container ">
+            <div className="p-5">
+
+            <div className="d-flex justify-content-between  mb-3" style={{margin:"0 40px"}}>
+                <h4 className="text-white">Support Portal</h4>
+                <p id="featured"  className="text-white fs-5"><a href="#">Track Tickets</a></p>
+            </div>
+
+            <div className="container m-3 p-3">
+                <div className="row ">
+                <div className="col-6 mb-2">
+                    <h4 className="text-white fs-4 ">Search for an answer or browse help topics to create a ticket</h4>
+                    <input className="m-2" placeholder="Eg:how do i activate F&O, why is my order getting late" id="support-inp"/>
+                    <div className="row text-center mt-2" id="track">
+                            <div className="col-md-4">
+                            <p className="text-white column"><a href="#">Track account opening</a></p>
+                            </div>
+                            <div className="col-md-4 ">
+                            <p className="text-white column"><a href="#"> Track segment activation</a></p>
+                            </div>
+                            <div className="col-md-4 ">
+                            <p className="text-white column"><a href="#">Intraday margins</a></p>
+                            </div>
+                            <div className="col-md-4 ">
+                            <p className="text-white column"><a href="#"> Kite user manual</a></p>
+                            </div>               
+                    </div>
+                </div>
+                
+                <div className="col-6 pt-4 ps-5" id="featured">
+                    <h4 className="text-white">Featured</h4>
+                    <div className="col">
+                    <p className="fs-6"><a href="#">1. Muhurat Trading session on account of Diwali,<br /> November 2024</a></p>
+                    </div>
+                    <div className="col">
+                    <p className="fs-6"><a href="#">2. Current Takeovers and Delisting - October 2024</a></p>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            </div>
+            </div>
+           
+        </div>
     );
 }
 
