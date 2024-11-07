41ab3314699d7fd63d03600490d42a4f5769f84c
Shaik Alina <shaikalina2004@gmail.com>
2024-11-07 23:03:36 +0530

    navbar

--- src/landing_page/Navbar.js
+++ src/landing_page/Navbar.js
@@ -3,13 +3,13 @@ import {Link} from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light border-bottom " style={{backgroundColor:"#fff"}}>
-      <div class="container p-2">
-        <Link class="navbar-brand " to="/">
+    <nav className="navbar navbar-expand-lg navbar-light border-bottom " style={{backgroundColor:"#fff"}}>
+      <div className="container p-2">
+        <Link className="navbar-brand " to="/">
           <img src="media/images/logo.svg" alt="logo" style={{width:"25%"}}/>
         </Link>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -17,34 +17,34 @@ function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
        
-          <form class="d-flex">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <Link class="nav-link active" aria-current="page" to="/signup">
+          <form className="d-flex">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/signup">
                 Signup
               </Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link active" to="/about">
+            <li className="nav-item">
+              <Link className="nav-link active" to="/about">
                 About
               </Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link active" to="/product">
+            <li className="nav-item">
+              <Link className="nav-link active" to="/product">
                 Product
               </Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link active" to="/pricing">
+            <li className="nav-item">
+              <Link className="nav-link active" to="/pricing">
                 Pricing
               </Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link active" to="/support">
+            <li className="nav-item">
+              <Link className="nav-link active" to="/support">
                 Support
               </Link>
             </li>
